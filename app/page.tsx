2dc04fe0b96d119d00e52040735aac322b7d113e
rjmacalino <rjhaymacalino@gmail.com>
2026-09-15 15:40:49 +1000

    PORT-11: add contact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
@@ -47,6 +48,7 @@ export default function Home() {
       <About />
       <Skills />
       <Projects />
+      <Contact />
     </>
   );
 }
